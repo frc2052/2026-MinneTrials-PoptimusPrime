feef2895b87f48ee8c70a29cf40e813903be4e2b
Vivienne143 <viviennemartin0923@gmail.com>
2025-12-01 19:23:29 -0600

    I made my commands and possibly finished the minne robot code???

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,15 +4,19 @@
 
 package frc.robot;
 
+import frc.robot.subsystems.Butter;
 import frc.robot.subsystems.DriveTrain;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.ArcadeDrive;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.Popcorn;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 /**
@@ -25,14 +29,14 @@ public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
-  // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
-      private final Joystick m_controller = new Joystick(0);
+  private final Joystick m_controller = new Joystick(0);
 
-      
-      private final DriveTrain m_drivetrain = new DriveTrain();
+  private final DriveTrain m_drivetrain = new DriveTrain();
 
+  private final Butter m_butter = new Butter();
+
+  private final Popcorn m_popcorn = new Popcorn();
+  
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the trigger bindings
@@ -54,9 +58,29 @@ public class RobotContainer {
         .onTrue(new ExampleCommand(m_exampleSubsystem));
         m_drivetrain.setDefaultCommand(getArcadeDriveCommand());
 
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
-    // cancelling on release.
-    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+    JoystickButton butterIntakeButton = new JoystickButton(m_controller, 3);
+    butterIntakeButton.whileTrue(new InstantCommand(() -> m_butter.butterIntake()))
+      .onFalse(new InstantCommand(() -> m_butter.stop()));
+
+    JoystickButton butterOutakeButton = new JoystickButton(m_controller, 5);
+    butterOutakeButton.whileTrue(new InstantCommand(() -> m_butter.butterOutake()))
+      .onFalse(new InstantCommand(() -> m_butter.stop()));
+
+    JoystickButton popcornIntakeButton = new JoystickButton(m_controller, 6);
+    popcornIntakeButton.whileTrue(new InstantCommand(() -> m_popcorn.popcornIntake()))
+      .onFalse(new InstantCommand(() -> m_popcorn.stopIntake()));
+
+    JoystickButton popcornOutakeButton = new JoystickButton(m_controller, 4);
+    popcornOutakeButton.whileTrue(new InstantCommand(() -> m_popcorn.popcornOutake()))
+      .onFalse(new InstantCommand(() -> m_popcorn.stopIntake()));
+
+    JoystickButton popcornShooterButton = new JoystickButton(m_controller, 1);
+    popcornShooterButton.whileTrue(new InstantCommand(() -> m_popcorn.shooter()))
+      .onFalse(new InstantCommand(() -> m_popcorn.stopShooter()));
+
+    JoystickButton popcornIndexerButton = new JoystickButton(m_controller, 2);
+    popcornIndexerButton.whileTrue(new InstantCommand(() -> m_popcorn.indexer()))
+      .onFalse(new InstantCommand(() -> m_popcorn.stopIndexer()));
   }
 
   /**
--- src/main/java/frc/robot/subsystems/Butter.java
+++ src/main/java/frc/robot/subsystems/Butter.java
@@ -4,14 +4,45 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Butter extends SubsystemBase {
   /** Creates a new Butter. */
   public Butter() {}
 
+  private WPI_TalonSRX butter_motor = new WPI_TalonSRX(2);
+
+
+
+  public void butterOutake() {
+
+     butter_motor.set(0.1);
+    
+    }
+
+
+  public void butterIntake() {
+
+    butter_motor.set(-0.1);
+
+  }
+
+  public void stop() {
+    butter_motor.set(0);
+  }
+
+
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
   }
 }
+
+
+
+/** I currently need to make butter's motor set to 0.1 or -0.1 for intake/outake 
+ * please use 
+  butter_motor.set(0.1); at some point
+*/
