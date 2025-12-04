1e1c3f3b5db6b5cefecdde88d4e0f77a1905cdad
saimanaswini122 <2029BeshS82@moundsviewschools.org>
2025-12-03 18:49:29 -0600

    MinnieTrialsBlue

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,9 +7,15 @@ package frc.robot;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
+import frc.robot.subsystems.Butter;
+import frc.robot.subsystems.DriveTrain;
 import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.Popcorn;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 /**
@@ -20,11 +26,12 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  private final ExampleSubsystem m_ExampleSubsystem = new ExampleSubsystem();
+  private final Butter m_ButterSubsystem = new Butter();
+  private final Popcorn m_PopcornSubsystem = new Popcorn();
+  private final DriveTrain m_DriveTrainSubsystem = new DriveTrain();
 
-  // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+  private final Joystick joystickOne = new Joystick(0);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
@@ -42,13 +49,23 @@ public class RobotContainer {
    * joysticks}.
    */
   private void configureBindings() {
-    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    new Trigger(m_exampleSubsystem::exampleCondition)
-        .onTrue(new ExampleCommand(m_exampleSubsystem));
+
 
     // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
     // cancelling on release.
-    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+    JoystickButton exampleButton = new JoystickButton(joystickOne, 7);
+    exampleButton.whileTrue(new InstantCommand(() -> m_ExampleSubsystem.exampleMethodCommand()))
+    .onFalse(new InstantCommand(() -> m_ExampleSubsystem.exampleMethodCommand()));
+
+    JoystickButton ButterButton = new JoystickButton(joystickOne, 3);
+    ButterButton.whileTrue(new InstantCommand(() -> m_ButterSubsystem.butterMethodCommand()))
+    .onFalse(new InstantCommand(() -> m_ButterSubsystem.stop()));
+
+    JoystickButton PopcornButton = new JoystickButton(joystickOne, 4);
+    PopcornButton.whileTrue(new InstantCommand(() -> m_PopcornSubsystem.popcornMethodCommand()))
+    .onFalse(new InstantCommand(() -> m_PopcornSubsystem.stop()));
+
+    m_drivetrain.setDefaultCommand();
   }
 
   /**
@@ -58,6 +75,6 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An example command will be run in autonomous
-    return Autos.exampleAuto(m_exampleSubsystem);
+    return Autos.exampleAuto(m_ExampleSubsystem);
   }
 }
