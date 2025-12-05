10619cd021f4559714a99dda37d896309b3f298c
josephy221 <josephy997@gmail.com>
2025-12-04 18:56:29 -0600

    commands ready to merge

--- src/main/java/frc/robot/commands/drive/TurnDegreesCommand.java
+++ src/main/java/frc/robot/commands/drive/TurnDegreesCommand.java
@@ -2,57 +2,58 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands.drive;
+package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.Constants.DriveTrain;
 import frc.robot.subsystems.DrivetrainSubsystem;
-import frc.robot.Constants.DrivetrainConstants;
 
+public class RotateSetDegrees extends Command {
+  private double degChangeReq;
+  private double initialDegreeVal;
+  private DrivetrainSubsystem drivetrain;
+  private boolean clockwise;
 
-/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
-public class TurnDegreesCommand extends Command {
-  private final DrivetrainSubsystem drivetrain;
-  private final double speed, degrees;
-
-  /** Creates a new TurnDegrees. */
-  public TurnDegreesCommand(double speed, double degrees, DrivetrainSubsystem drivetrain) {
+  /** Creates a new RotateSetDegrees. */
+  public RotateSetDegrees(DrivetrainSubsystem drivetrain, double degreesRequested, boolean clockwise) {
     this.drivetrain = drivetrain;
-    this.speed = speed;
-    this.degrees = degrees;
-    // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(drivetrain);
+    degChangeReq = degreesRequested;
+    initialDegreeVal = 0.0;
+    this.clockwise = clockwise;
   }
 
-  // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    drivetrain.arcadeDrive(0,0);
-    drivetrain.resetEncoders();
+      initialDegreeVal = drivetrain.getGyroAngleDegrees();
   }
 
-  // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {
-    drivetrain.arcadeDrive(0, speed);
+  public void execute() { // add math for direction
+    
+    if(clockwise == true){
+      drivetrain.arcadeDrive(0, -0.7);
+    } else {
+      drivetrain.arcadeDrive(0, 0.7);
+    }
   }
 
-  // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
     drivetrain.arcadeDrive(0, 0);
+    new InstantCommand(() -> drivetrain.zeroAll());
   }
 
-  // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    double inchPerDegree = DrivetrainConstants.WHEEL_CIRCUMFERENCE / 360;
-    return getAverageTurningDistance() >= inchPerDegree * degrees;
-  }
-
-  private double getAverageTurningDistance() {
-    double leftDistance = Math.abs(drivetrain.getLeftEncoderPos());
-    double rightDistance = Math.abs(drivetrain.getRightEncoderPos());
-    return (leftDistance + rightDistance) / 2.0;
+    System.out.println("GYRO DEGREES: " + drivetrain.getGyroAngleDegrees());
+    System.out.println("REQUEST DEGREE: " + initialDegreeVal + degChangeReq);
+    if(drivetrain.getGyroAngleDegrees() < (initialDegreeVal + degChangeReq)){
+      return false;
+    } else {
+      System.out.println("===== STOP ROTATING");
+      return true;
+    }
   }
 }
 
--- src/main/java/frc/robot/commands/drive/ZeroGyroCommand.java
+++ src/main/java/frc/robot/commands/drive/ZeroGyroCommand.java
@@ -8,17 +8,11 @@ import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.DrivetrainSubsystem;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
-public class DriveDistanceCommand extends Command {
-  private final double speed, distance;
+public class ZeroGyroCommand extends Command {
   private final DrivetrainSubsystem drivetrain;
-  private final boolean isFacingForward;
-
-  /** Creates a new DriveDistance. */
-  public DriveDistanceCommand(double speed, boolean isFacingForward, double ft, DrivetrainSubsystem drivetrain) {
+  /** Creates a new ZeroGyro. */
+  public ZeroGyroCommand(DrivetrainSubsystem drivetrain) {
     this.drivetrain = drivetrain;
-    this.isFacingForward = isFacingForward;
-    this.distance = ft;
-    this.speed = speed;
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(drivetrain);
   }
@@ -26,29 +20,20 @@ public class DriveDistanceCommand extends Command {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    drivetrain.arcadeDrive(0.0,0.0);
-    drivetrain.resetEncoders();
-    drivetrain.resetOdometry();
+    drivetrain.zeroGyro();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {
-    if(isFacingForward) {
-      drivetrain.arcadeDrive(speed,0.0); }
-    else {
-      drivetrain.arcadeDrive(-speed, 0);
-    }
-  }
+  public void execute() {}
+
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {
-    drivetrain.arcadeDrive(0.0,0.0);
-  }
+  public void end(boolean interrupted) {}
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return Math.abs(drivetrain.getAverageEncoderDistance()) >= distance;
+    return false;
   }
 }
